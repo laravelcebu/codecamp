aa15cb8e38fe474bc6776169135d7ed2836ca897
Journey Dagoc <jmdhagz@gmail.com>
2018-05-12 15:30:01 +0800

    attendee:journey-dagoc

---
+++ resources/views/attendees/dagoc-journey.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div style="width: 75%; margin: 0 auto; padding-top: 100px;">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="col-md-3">
+					<img src="https://lh3.googleusercontent.com/-Mk809ZOkW9Q/VMX6z_SRRLI/AAAAAAAAAKs/DQaKpZlvMLAOuP2xyXNVaMknggavC9J-QCEwYBhgL/w140-h139-p/1001315_672561629426507_210711918_n.jpg"  class="img-thumbnail">
+				</div>
+				<div class="col-md-9">
+					<h1 style="font-size: 50px"><strong>JOURNEY DAGOC</strong></h1>
+					<p><i>Web Developer</i></p>
+				</div>
+			</div>
+		</div>
+		<div class="row" style="margin-top: 50px;">
+			<div class="col-md-12">
+				<div class="col-md-6">
+					<div class="form-group">
+						<h2><strong>Email Address</strong><br> jmdhagz@gmail.com</h2>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group" style="text-align: right;">
+						<h2><strong>Mobile Number</strong><br> 09153325937</h2>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="col-md-6">
+					<div class="form-group">
+						<h2><strong>Skills</strong><br> PHP, Javascripts, VueJS</h2>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group" style="text-align: right;">
+						<h2><strong>Gender</strong><br> Male</h2>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-12">
+				<div class="col-md-6">
+					<div class="form-group">
+						<h2><strong>Birthdate</strong><br> May 24, 1995</h2>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="form-group" style="text-align: right;">
+						<h2><strong>Religion</strong><br> Roman Catholic</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
