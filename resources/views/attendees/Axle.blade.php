632c2d4bacf44efde790a5cbc40fafe307893ebf
Dorell James Galang <galangdj@gmail.com>
2018-05-12 16:33:01 +0800

    updates

---
+++ resources/views/attendees/Axle.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-sm-50">
+			<br>
+			<h2>TITA YUNG ANAK NYO PO!!...</h2>
+		</br>
+			
+			<img class="irc_mi" src="http://comosur.com/wp-content/uploads/2013/10/409844894_bff4f46bdb_z-e1381423915342.jpg" alt="Image result for eating cat" onload="typeof google==='object'&amp;&amp;google.aft&amp;&amp;google.aft(this)" width="400" height="345" style="margin-top: 4px;">
+
+			<br>
+			<h2>KUMAIN NG PUSA!!.</h2>
+		</br>
+
+		</div>
+	</div>
+</div>s
+@endsection
---
+++ resources/views/attendees/dorell-james.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+
+			<h3>DORELL JAMES GALANG</h3>
+			
+			<iframe src="https://giphy.com/embed/uINwDsSVEfGsE" width="270" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/baby-director-groot-uINwDsSVEfGsE">via GIPHY</a></p>
+
+		</div>
+	</div>
+</div>
+@endsection
