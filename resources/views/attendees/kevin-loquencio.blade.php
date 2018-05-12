9a39eac5bb2797d02ec002162bbc4d4d2ab270f3
babojamo <kevin.loquencio@gmail.com>
2018-05-12 15:22:40 +0800

    kevin loquencio profile commit.

--- resources/views/attendees/kevin-loquencio.blade.php
+++ resources/views/attendees/kevin-loquencio.blade.php
@@ -4,12 +4,14 @@
 
 
 
+
 <div class="panel panel-default">
 
     <div class="panel-body">
       
       <h1>Kevin Loquencio</h1>
-<p>Age: Secret</p>
+        <p>Age: Secret</p>
+        <a href="https://www.facebook.com/breadasia">Facebook</a> | <a href="https://www.linkedin.com/in/kevin-loquencio-265073162/">linkedin</a>
     </div>
 </div>
 
