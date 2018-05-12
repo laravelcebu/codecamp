6ca3511b1203d992e60b15527671515498ef7004
Harlequin Doyon <harlequin.doyon@gmail.com>
2018-05-12 14:27:16 +0800

    Added attendees route

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/activation/{token}', 'ActivationController@index')->name('activate');
 
 Route::resource('users', '\App\Http\Controllers\UserController');
+
+Route::get('attendees/{name}', '\App\Http\Controllers\AttendeeController');
